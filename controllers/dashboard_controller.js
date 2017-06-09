682d2dfe8e00fd1c6ee713da4d1a9b712de4ca9e
Jonah George <jonah.george@me.com>
2017-06-08 17:04:27 -0700

    Remove unnecessary queries from DashboardController#show

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -1,7 +1,5 @@
 const SiteRepository = require('../repositories/site_repository.js');
-const VisitRepository = require('../repositories/visit_repository.js');
 const UserRepository = require('../repositories/user_repository.js');
-const VisitsPerMinuteQuery = require('../queries/visits_per_minute_query.js');
 
 class DashboardController {
   static async index(request, response) {
@@ -18,15 +16,9 @@ class DashboardController {
   
   static async show(request, response) {
     const siteId = request.params["siteId"];
-    const visitsPerMinuteQuery = new VisitsPerMinuteQuery(request.app.pool);
-  
-    const hourlyVisits = await visitsPerMinuteQuery.run(siteId, 'hour', 'minute');
-    const dailyVisits = await visitsPerMinuteQuery.run(siteId, 'day', 'hour');
   
     response.render('dashboard/show', {
       siteId: siteId,
-      hourlyVisits: JSON.stringify(hourlyVisits),
-      dailyVisits: JSON.stringify(dailyVisits),
     });
   };
 }
